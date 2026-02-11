3cf0295a89c23255b8285c70bac121499e63ffd9
AsdruvalTejada <adtp0003@ce.pucmm.edu.do>
2026-02-10 20:40:01 -0400

    Respuesta teóricas

--- src/PrintManager.java
+++ src/PrintManager.java
@@ -33,4 +33,19 @@ public class PrintManager {
 
 //Respuesta a preguntas teóricas:
 //1. Para cada opción (A y B), determine la complejidad asintótica de las operaciones:
-//
+//Opción A (Tres colas simples):
+//Esta es enqueue: O(1).
+//Simplemente tenemos que mirar la letra y lo agregamos al final (tail) de una de las 3 listas
+//Esto sería O(1).
+//Luego verificamos si H está vacía, si no M, si no L, esas son 3 verificaciones constantes.
+
+//Opción B (Una lista enlazada ordenada):
+//Esta es enqueue: O(N).
+//En el peor caso, se tendría que recorrer toda la lista para ponerlo al final (esto para una priority de LOW).
+//dequeue: O(1). Siempre sacas el primero (head).
+
+//2. Complejidad Total para N trabajos:
+// Opción A: Insertar N veces cuesta N*1 = O(N) y procesar N veces cuesta N*1 = O(N).
+// El total sería: O(N).
+// Opción B: Insertar N veces cuesta (1 + 2 + ... + N) = O(N^2). Procesar N veces cuesta O(N).
+// Su total es: O(N^2).
